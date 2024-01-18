8bd222faaa777c4bbae73f80d0e89fce2f228e65
mwdorji <94346653+mwdorji@users.noreply.github.com>
2024-01-17 19:23:54 -0700

    Session 3

---
+++ Session3/src/Child.java
@@ -0,0 +1,12 @@
+
+public class Child extends Parent{
+	//overloading
+	void add(long a, long b) {
+		long c = a+b;
+		System.out.println(c);
+	}
+	void show() {
+		System.out.println("I am child");
+	}
+
+}
---
+++ Session3/src/TestFour.java
@@ -0,0 +1,14 @@
+
+public class TestFour {
+	public static void main(String[] args) {
+		Parent parent = new Parent();
+		parent.add(1,3);
+		parent.show();
+		
+		Child child = new Child();
+		child.add(1, 5);
+		child.show();
+		
+	}
+
+}

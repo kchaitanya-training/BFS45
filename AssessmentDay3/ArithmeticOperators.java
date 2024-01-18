16a890510f7002526acd1dc2e430ea20bc902f62
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-01-18 12:53:27 -0600

    arithmetic operators assessment

---
+++ AssessmentDay3/ArithmeticOperators.java
@@ -0,0 +1,20 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 10;
+
+        a++;
+        System.out.println(a);
+
+        a--;
+        System.out.println(a);
+
+        a = a / 2;
+        System.out.println(a);
+
+        a = a * 2;
+        System.out.println(a);
+
+        a = a % 2;
+        System.out.println(a);
+    }
+}

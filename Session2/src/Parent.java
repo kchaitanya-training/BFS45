0fc33a24615700054e1e5f335425cd80a7bfe817
mwdorji <94346653+mwdorji@users.noreply.github.com>
2024-01-17 19:27:23 -0700

    Session 2

---
+++ Session2/src/Parent.java
@@ -0,0 +1,14 @@
+
+public class Parent {
+	// Overloading
+	void add(int a, int b) {
+		int c = a+b;
+		System.out.println(c);
+	}
+	//override
+	void show() {
+		System.out.println("I am parent");
+	}
+	
+	
+}

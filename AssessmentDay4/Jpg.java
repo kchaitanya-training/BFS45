79dc0190d5b2a50941897e7613ac591af604f348
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-01-19 12:43:35 -0600

    implement abstract class methods

---
+++ AssessmentDay4/Jpg.java
@@ -0,0 +1,11 @@
+package AssessmentDay4;
+
+public class Jpg extends CustomFile {
+    Jpg() {
+        this.extension = ".jpg";
+    }
+
+    public void readFile() {
+        System.out.println("Reading JPG File");
+    }
+}
---
+++ AssessmentDay4/Pdf.java
@@ -0,0 +1,11 @@
+package AssessmentDay4;
+
+public class Pdf extends CustomFile {
+    Pdf() {
+        this.extension = ".pdf";
+    }
+
+    public void readFile() {
+        System.out.println("Reading PDF File");
+    }
+}

39f9c22c8b429b171275931982c92964058f26df
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-01-24 12:52:21 -0600

    generics

---
+++ AssessmentDay7/generic/Main.java
@@ -0,0 +1,18 @@
+package AssessmentDay7.generic;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> primeNumbers = new ArrayList<Integer>();
+
+        primeNumbers.add(2);
+        primeNumbers.add(3);
+        primeNumbers.add(5);
+        primeNumbers.add(7);
+        primeNumbers.add(11);
+        primeNumbers.add(13);
+
+        System.out.println(primeNumbers);
+    }
+}

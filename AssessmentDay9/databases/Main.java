2444af86fe53980aaab8a72e618ac07c93f565d2
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-01-26 10:29:21 -0600

    updated driver code

--- AssessmentDay9/databases/Main.java
+++ AssessmentDay9/databases/Main.java
@@ -5,23 +5,39 @@ import java.util.ArrayList;
 import java.sql.SQLException;
 
 public class Main {
-    void createAndInsert(DatabaseOperations db) throws SQLException {
+    DatabaseOperations db;
+
+    Main(DatabaseOperations db) {
+        this.db = db;
+    }
+
+    void createAndInsert() throws SQLException {
         List<Employee> employees = new ArrayList<Employee>();
 
         employees.add(new Employee(1, "Tom", "CA", 10000.0f));
         employees.add(new Employee(2, "Mark", "CA", 10000.0f));
         employees.add(new Employee(3, "Travis", "CA", 10000.0f));
 
-        db.insertRecord(employees);
+        this.db.insertRecord(employees);
+    }
+
+    void updateRecord() throws SQLException {
+        Employee em = new Employee(1, "Tom", "CA", 50000.45f);
+
+        this.db.updateRecord(em);
     }
 
     public static void main(String[] args) {
         DatabaseOperations db = new DatabaseOperations("takeo", "root");
+        Main m = new Main(db);
 
         try {
             db.connectDB();
 
             db.listRecord();
+
+            // m.createAndInsert();
+            // m.updateRecord();
 
             db.disconnectDB();
         } catch (Exception e) {

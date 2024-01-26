e1e33ba1e5f76b4f45ef6a904844a33ac535ebf5
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-01-26 09:30:41 -0600

    employee data model class

---
+++ AssessmentDay9/databases/Employee.java
@@ -0,0 +1,47 @@
+package AssessmentDay9.databases;
+
+public class Employee {
+    int id;
+    String name;
+    String address;
+    float salary;
+
+    Employee(int id, String name, String address, float salary) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+}

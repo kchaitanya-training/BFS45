4f92eea1ab71fc364dd0e72d8a07aa6e5c1e2443
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-01-26 10:06:13 -0600

    database driver code

---
+++ AssessmentDay9/databases/Main.java
@@ -0,0 +1,29 @@
+package AssessmentDay9.databases;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.SQLException;
+
+public class Main {
+    void createAndInsert(DatabaseOperations db) throws SQLException {
+        List<Employee> employees = new ArrayList<Employee>();
+
+        employees.add(new Employee(1, "Tom", "CA", 10000.0f));
+        employees.add(new Employee(2, "Mark", "CA", 10000.0f));
+        employees.add(new Employee(3, "Travis", "CA", 10000.0f));
+
+        db.insertRecord(employees);
+    }
+
+    public static void main(String[] args) {
+        DatabaseOperations db = new DatabaseOperations("takeo", "root");
+
+        try {
+            db.connectDB();
+
+            db.listRecord();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
